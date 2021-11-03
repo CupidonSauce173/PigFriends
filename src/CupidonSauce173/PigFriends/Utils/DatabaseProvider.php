ea9c4a4227bcc9ed5dcd78b8d4b6888e50b26edb
CupidonSauce173 <CupidonSauce173@gmail.com>
2021-11-03 15:54:35 -0400

    Typo

--- src/CupidonSauce173/PigFriends/Utils/DatabaseProvider.php
+++ src/CupidonSauce173/PigFriends/Utils/DatabaseProvider.php
@@ -6,6 +6,7 @@ namespace CupidonSauce173\PigFriends\Utils;
 use CupidonSauce173\PigFriends\FriendsLoader;
 use function mysqli_connect;
 use function mysqli_query;
+use function mysqli_close;
 use function mysqli_select_db;
 
 class DatabaseProvider
@@ -38,7 +39,7 @@ class DatabaseProvider
             FriendsLoader::getInstance()->getServer()->shutdown();
         }
 
-        $s_db = mysqli_select_db($this->sqlInfo['database'], $db);
+        $s_db = mysqli_select_db($db, $this->sqlInfo['database']);
         if (!$s_db) {
             $query = "CREATE DATABASE ?";
             $stmt = $db->prepare($query);
@@ -48,7 +49,7 @@ class DatabaseProvider
             var_dump('Database ' . $this->sqlInfo['database'] . ' has been created.');
         }
 
-        mysqli_query('
+        mysqli_query($db, '
         CREATE TABLE IF NOT EXISTS FriendsConfigs(
             sector VARCHAR(255) NOT NULL,
             notify BOOLEAN NOT NULL,
@@ -56,8 +57,8 @@ class DatabaseProvider
             request_check_time INT NOT NULL,
             friend_hits BOOLEAN NOT NULL
         ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
-        ', $db);
-        mysqli_query('
+        ');
+        mysqli_query($db, '
         CREATE TABLE IF NOT EXISTS FriendSettings(
            player VARCHAR(15) NOT NULL,
            request_state BOOLEAN NOT NULL DEFAULT TRUE,
@@ -65,8 +66,8 @@ class DatabaseProvider
            join_message INT NOT NULL DEFAULT 0,
            PRIMARY KEY (player)
         ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
-        ', $db);
-        mysqli_query('
+        ');
+        mysqli_query($db, '
         CREATE TABLE IF NOT EXISTS FriendRequests(
            id MEDIUMINT NOT NULL AUTO_INCREMENT,
            sender VARCHAR(15) NOT NULL,
@@ -76,8 +77,8 @@ class DatabaseProvider
            FOREIGN KEY (sender) REFERENCES FriendSettings(player),
            FOREIGN KEY (receiver) REFERENCES FriendSettings(player)
         ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
-        ', $db);
-        mysqli_query('
+        ');
+        mysqli_query($db, '
         CREATE TABLE IF NOT EXISTS FriendRelations(
            id MEDIUMINT NOT NULL AUTO_INCREMENT,
            base_player VARCHAR(15) NOT NULL,
@@ -87,14 +88,15 @@ class DatabaseProvider
            FOREIGN KEY (base_player) REFERENCES FriendSettings(player),
            FOREIGN KEY (friend) REFERENCES FriendSettings(player)
         ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
-        ', $db);
-        mysqli_query('
+        ');
+        mysqli_query($db, '
         CREATE TABLE IF NOT EXISTS RelationState(
            relation_id MEDIUMINT NOT NULL,
            is_favorite BOOLEAN NOT NULL DEFAULT FALSE,
            is_blocked BOOLEAN NOT NULL DEFAULT FALSE,
            FOREIGN KEY (relation_id) REFERENCES FriendRelations(id)
         ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
-        ', $db);
+        ');
+        mysqli_close($db);
     }
 }
