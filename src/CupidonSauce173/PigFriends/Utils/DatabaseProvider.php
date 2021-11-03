c985d61a5752277e15751bbffca4cfbe99e73f93
CupidonSauce173 <CupidonSauce173@gmail.com>
2021-11-03 16:03:47 -0400

    Update DatabaseProvider.php

--- src/CupidonSauce173/PigFriends/Utils/DatabaseProvider.php
+++ src/CupidonSauce173/PigFriends/Utils/DatabaseProvider.php
@@ -4,10 +4,14 @@
 namespace CupidonSauce173\PigFriends\Utils;
 
 use CupidonSauce173\PigFriends\FriendsLoader;
-use function mysqli_connect;
-use function mysqli_query;
 use function mysqli_close;
+use function mysqli_connect;
+use function mysqli_prepare;
+use function mysqli_query;
 use function mysqli_select_db;
+use function mysqli_stmt_bind_param;
+use function mysqli_stmt_execute;
+use function var_dump;
 
 class DatabaseProvider
 {
@@ -27,29 +31,24 @@ class DatabaseProvider
      */
     function createDatabaseStructure(): void
     {
-        $db = mysqli_connect(
+        $link = mysqli_connect(
             $this->sqlInfo['ip'],
             $this->sqlInfo['user'],
             $this->sqlInfo['password'],
             null,
             $this->sqlInfo['port']
         );
-        if ($db->connect_error) {
-            FriendsLoader::getInstance()->getLogger()->error($db->connect_error);
+        if ($link->connect_error) {
+            FriendsLoader::getInstance()->getLogger()->error($link->connect_error);
             FriendsLoader::getInstance()->getServer()->shutdown();
         }
-
-        $s_db = mysqli_select_db($db, $this->sqlInfo['database']);
+        $s_db = mysqli_select_db($link, $this->sqlInfo['database']);
         if (!$s_db) {
-            $query = "CREATE DATABASE ?";
-            $stmt = $db->prepare($query);
-            $stmt->bind_param('s', $this->sqlInfo['database']);
-            $stmt->execute();
-            $stmt->close();
+            mysqli_query($link, 'CREATE DATABASE ' . $this->sqlInfo['database']);
             var_dump('Database ' . $this->sqlInfo['database'] . ' has been created.');
         }
 
-        mysqli_query($db, '
+        mysqli_query($link, '
         CREATE TABLE IF NOT EXISTS FriendsConfigs(
             sector VARCHAR(255) NOT NULL,
             notify BOOLEAN NOT NULL,
@@ -58,7 +57,7 @@ class DatabaseProvider
             friend_hits BOOLEAN NOT NULL
         ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
         ');
-        mysqli_query($db, '
+        mysqli_query($link, '
         CREATE TABLE IF NOT EXISTS FriendSettings(
            player VARCHAR(15) NOT NULL,
            request_state BOOLEAN NOT NULL DEFAULT TRUE,
@@ -67,7 +66,7 @@ class DatabaseProvider
            PRIMARY KEY (player)
         ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
         ');
-        mysqli_query($db, '
+        mysqli_query($link, '
         CREATE TABLE IF NOT EXISTS FriendRequests(
            id MEDIUMINT NOT NULL AUTO_INCREMENT,
            sender VARCHAR(15) NOT NULL,
@@ -78,7 +77,7 @@ class DatabaseProvider
            FOREIGN KEY (receiver) REFERENCES FriendSettings(player)
         ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
         ');
-        mysqli_query($db, '
+        mysqli_query($link, '
         CREATE TABLE IF NOT EXISTS FriendRelations(
            id MEDIUMINT NOT NULL AUTO_INCREMENT,
            base_player VARCHAR(15) NOT NULL,
@@ -89,7 +88,7 @@ class DatabaseProvider
            FOREIGN KEY (friend) REFERENCES FriendSettings(player)
         ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
         ');
-        mysqli_query($db, '
+        mysqli_query($link, '
         CREATE TABLE IF NOT EXISTS RelationState(
            relation_id MEDIUMINT NOT NULL,
            is_favorite BOOLEAN NOT NULL DEFAULT FALSE,
@@ -97,6 +96,6 @@ class DatabaseProvider
            FOREIGN KEY (relation_id) REFERENCES FriendRelations(id)
         ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
         ');
-        mysqli_close($db);
+        mysqli_close($link);
     }
 }
